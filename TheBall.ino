b856647db22439281c77e9745f4f016869f35d9e
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-12-01 18:11:59 +0100

    Add project files.

---
+++ TheBall.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TheBall.ino
+ Created:	12/1/2017 6:11:54 PM
+ Author:	Dr. Jochen Manns
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
