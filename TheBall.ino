c0c3cdfe8939f2cddb5899714628bbf0c141bd05
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-12-05 23:50:21 +0100

    Finished animations

--- TheBall.ino
+++ TheBall.ino
@@ -13,12 +13,18 @@
 
 IRrecv irrecv(REMOTE_PIN);
 
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_CONTROL, NEO_RGBW + NEO_KHZ800);
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, LED_CONTROL, NEO_GRBW + NEO_KHZ800);
 
 // Aktuelle Visualisierung.
-int mode = 1;
+uint32_t COLOR_WHITE = Adafruit_NeoPixel::Color(255, 255, 255, 255);
+uint32_t COLOR_RED = Adafruit_NeoPixel::Color(255, 0, 0, 0);
+uint32_t COLOR_BLACK = Adafruit_NeoPixel::Color(0, 0, 0, 0);
+
+int mode = 0;
 
 void setup() {
+    randomSeed(analogRead(0));
+
     // LED Ring initialisieren.
     leds.begin();
 
@@ -46,7 +52,7 @@ void loop() {
         switch (results.value)
         {
         default:
-            mode = 1;
+            mode = 0;
             break;
         }
 
@@ -56,30 +62,73 @@ void loop() {
 
     // Anzeige.
     switch (mode) {
+    case 0:
+        black();
+        break;
     case 1:
-        showMode1();
+        white();
+        break;
+    case 2:
+        rotate();
+        break;
+    case 3:
+        search();
+        break;
+    case 4:
+        glitter();
         break;
     }
 }
 
-void showMode1() {
-    for (auto led = 0; led < 7; led++) {
-        for (auto what = 0; what < 4; what++) {
-            leds.clear();
-            leds.setPixelColor(led, Adafruit_NeoPixel::Color(
-                (what == 0) ? 255 : 0,
-                (what == 1) ? 255 : 0,
-                (what == 2) ? 255 : 0,
-                (what == 3) ? 255 : 0
-            ));
-            leds.show();
-
-            delay(1000);
-        }
-    }
-
+void black() {
     leds.clear();
     leds.show();
+}
 
-    delay(10000);
+void white() {
+    for (auto led = LED_COUNT; led-- > 0;) {
+        leds.setPixelColor(led, COLOR_WHITE);
+    }
+
+    leds.show();
+}
+
+void rotate() {
+    leds.setPixelColor(0, COLOR_RED);
+
+    for (auto count = LED_COUNT; count-- > 1; )
+    {
+        for (auto led = LED_COUNT; led-- > 1; ) {
+            leds.setPixelColor(led, (count == led) ? COLOR_RED : COLOR_BLACK);
+        }
+
+        leds.show();
+
+        delay(50);
+    }
+}
+
+int sequence[] = { 1, 4, 2, 5, 3, 6 };
+
+void glitter() {
+    for (auto led = 0; led < LED_COUNT; led++) {
+        leds.setPixelColor(led, Adafruit_NeoPixel::Color(50 + random(100), 50 + random(100), 50 + random(100), 0));
+        leds.show();
+    }
+
+    delay(100);
+}
+
+void search() {
+    for (int i = 0; i < sizeof(sequence) / sizeof(sequence[0]); i++) {
+        leds.clear();
+        leds.setPixelColor(0, COLOR_RED);
+        leds.show();
+        delay(120);
+
+        leds.clear();
+        leds.setPixelColor(sequence[i], COLOR_RED);
+        leds.show();
+        delay(120);
+    }
 }
